c6c449c327e38f6fc00c5d520de87717c782bba1
Martin Pezet <mpezet@ntlworld.com>
2022-12-15 14:58:35 +0000

    Added All page components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import { Footer, Header, Blog, Features, Possibility, WhatGPT } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
 const App = () => {
   return (
-    <section>
-        Hello
+    <section className='App'>
+      <div className="gradientBG">
+        <Navbar />
+        <Header />
+
+      </div>
+
+      <Brand />
+      <WhatGPT />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </section>
   )
 }
