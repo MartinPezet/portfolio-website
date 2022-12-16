8a59893ceb8d8e0a728367ac9d13008c9cdfd294
Martin Pezet <mpezet@ntlworld.com>
2022-12-16 19:54:31 +0000

    Added About section

--- client/src/components/brand/Brand.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Brand = () => {
-  return (
-    <section>Brand</section>
-  )
-}
-
-export default Brand
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
+export { default as About } from "./about/About";
 export { default as Article } from "./article/Article";
 export { default as Navbar } from "./navbar/Navbar";
