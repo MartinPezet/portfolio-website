647ab3837da1b60c1e886a06ab0c74c141ca98e0
Martin Pezet <mpezet@ntlworld.com>
2022-12-16 15:11:26 +0000

    Added Socials

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -1,3 +1,4 @@
 export { default as Blog } from './blog/Blog';
+export { default as Socials } from './socials/Socials';
 export { default as Header } from './header/Header';
 export { default as Footer } from './footer/Footer';
