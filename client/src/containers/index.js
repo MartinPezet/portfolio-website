508ff3d7d93b53cbad2456833b12948efcb14282
Martin Pezet <mpezet@ntlworld.com>
2022-12-16 19:52:26 +0000

    Added Projects and SiteFeatures

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -1,4 +1,5 @@
 export { default as SiteFeatures } from './siteFeatures/SiteFeatures';
+export { default as Projects } from './projects/Projects';
 export { default as Socials } from './socials/Socials';
 export { default as Header } from './header/Header';
 export { default as Footer } from './footer/Footer';
