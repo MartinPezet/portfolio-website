b64c1f6ab61e98a00c7adf8eb1cfa3108e47f100
Martin Pezet <mpezet@ntlworld.com>
2022-12-23 18:20:22 +0000

    Adding more about me

--- client/src/containers/aboutMe/AboutMe.js
+++ client/src/containers/aboutMe/AboutMe.js
@@ -7,8 +7,14 @@ import './aboutMe.css';
 const AboutMe = () => {
   return (
     <section className="aboutMe">
-        <div>
+        <div className="aboutMeHeader">
             <About title="About Me" text="I have been fascinated with Software Engineering from the age of 13 and have thoroughly enjoyed the journey through academia using a large array of languages and techniques for development. I am now embarking on my own Software Development journey and believe that, due to my ambition to learn, I would be of value to any team." />
+        </div>
+
+        <div className="aboutMeContent">
+          <About title="Education" text="" />
+          <About title="Work Experience" text="" />
+          <About title="Previous Projects" text="" />
         </div>
     </section>
   )
