2be28cb0bce7fbe136963e257fb0460371b40b69
Martin Pezet <mpezet@ntlworld.com>
2022-12-31 13:07:15 +0000

    Footer Styling Complete

--- client/src/containers/footer/Footer.js
+++ client/src/containers/footer/Footer.js
@@ -7,22 +7,26 @@ const Footer = () => {
   return (
     <section className="footer">
       <div className="footerContent">
-        <div className="footerContentTitle">
-          <h1>Martin Pezet</h1>
-          <h2>Portfolio Website</h2>
+        <div className="footerContentContainer1">
+          <div className="footerContentTitle">
+            <h1>Martin Pezet</h1>
+            <h2>Portfolio Website</h2>
+          </div>
+          <div className="footerContentMore">
+            <h2>More to do</h2>
+            <a href='#projects'>Visit my projects</a>
+            <a href='#downloadCV'>Request a CV</a>
+          </div>
         </div>
-        <div className="footerContentMore">
-          <h2>More to do</h2>
-          <a href='#projects'>Visit my projects</a>
-          <a href='#downloadCV'>Request a CV</a>
-        </div>
-        <div className="footerContentContact">
-          <h2>Contact Me</h2>
-          <a href='#email-me'>Email (coming soon)</a>
-        </div>
-        <div className="footerContentBackToTop">
-          <h2>Back to top</h2>
-          <a href='#navbar'>{<RiArrowUpLine />}</a>
+        <div className="footerContentContainer2">
+          <div className="footerContentContact">
+            <h2>Contact Me</h2>
+            <a href='#email-me'>Email (coming soon)</a>
+          </div>
+          <div className="footerContentBackToTop">
+            <h2>Back to top</h2>
+            <a href='#navbar'>{<RiArrowUpLine />}</a>
+          </div>
         </div>
       </div>
       <div className="footerCopyright">
