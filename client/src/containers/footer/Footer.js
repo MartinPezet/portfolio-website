c759515887b6e9ba50a3bd69cc862c86b74ad219
Martin Pezet <mpezet@ntlworld.com>
2022-12-31 12:33:28 +0000

    Initial Footer

--- client/src/containers/footer/Footer.js
+++ client/src/containers/footer/Footer.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import './footer.css';
+
+import { RiArrowUpLine } from 'react-icons/ri';
 
 const Footer = () => {
   return (
-    <section>Footer</section>
+    <section className="footer">
+      <div className="footerContent">
+        <div className="footerContentTitle">
+          <h1>Martin Pezet</h1>
+          <h2>Portfolio Website</h2>
+        </div>
+        <div className="footerContentMore">
+          <h2>More to do</h2>
+          <a href='#projects'>Visit my projects</a>
+          <a href='#downloadCV'>Request a CV</a>
+        </div>
+        <div className="footerContentContact">
+          <h2>Contact Me</h2>
+          <a href='#email-me'>Email (coming soon)</a>
+        </div>
+        <div className="footerContentBackToTop">
+          <h2>Back to top</h2>
+          <a href='#navbar'>{<RiArrowUpLine />}</a>
+        </div>
+      </div>
+      <div className="footerCopyright">
+        <p>Copyright &copy; {(new Date().getFullYear())} Martin Pezet</p>
+      </div>
+    </section>
   )
 }
 
