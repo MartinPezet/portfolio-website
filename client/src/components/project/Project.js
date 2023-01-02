153a4031c53aaf94898b18a411d0edfd7c984cdf
Martin Pezet <mpezet@ntlworld.com>
2023-01-02 15:17:16 +0000

    Adding Single Project Functionality

--- client/src/components/project/Project.js
+++ client/src/components/project/Project.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
-const Project = () => {
+import './project.css'
+
+const Project = ({direction, title, image, imgAlt, text}) => {
+
+  const projectSide = {color: "blue"}
+
+  useEffect(() => {
+      if (direction == "left") {
+        // Set css for left
+        let alignment = "left";
+      } else if (direction == "right"){
+        projectSide = {color: "red"}
+      }
+  }, (direction));
+
   return (
-    <section>Single Project</section>
+    <section className="project">
+      <div style={projectSide}>
+        {title}
+      </div>
+      <div>
+        <img src={image} alt={imgAlt}/>
+      </div>
+      <div>
+        <p>{text}</p>
+      </div>
+    </section>
   )
 }
 
--- client/src/containers/projects/Projects.js
+++ client/src/containers/projects/Projects.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import './projects.css';
+import { Project } from '../../components'
+
+// Images
+import img1 from '../../assets/GitHub.png';
 
 const Projects = () => {
   return (
     <section>
       Projects
-
-      <p>Real Estate Supervision</p>
+      <Project direction="right" title="Real Estate Supervisor" text="A real estate management system." image={img1} imgAlt="Github Logo" />
 
     </section>
   )
