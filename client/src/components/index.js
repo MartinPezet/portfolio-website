2f54805ce427c28a05cbec2c8aae1f07b92b9378
Martin Pezet <mpezet@ntlworld.com>
2023-01-05 14:45:05 +0000

    Added Popup Component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as About } from "./about/About";
 export { default as Project } from "./project/Project";
 export { default as Navbar } from "./navbar/Navbar";
+export { default as Popup } from "./popup/Popup"
