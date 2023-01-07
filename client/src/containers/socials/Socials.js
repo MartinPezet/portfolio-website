8884beb7ba528849955de41be385a744f629751d
Martin Pezet <mpezet@ntlworld.com>
2023-01-07 14:22:32 +0000

    Added scaleOnHover

--- client/src/containers/socials/Socials.js
+++ client/src/containers/socials/Socials.js
@@ -7,10 +7,10 @@ const Socials = () => {
   return (
     <section className="socials">
       <div className="socialsContainer">
-        <a href="https://github.com/MartinPezet" className="socialItem">
+        <a href="https://github.com/MartinPezet" className="socialItem scaleOnHover">
           <img src={github} alt="Github Logo" />
         </a>
-        <a href="https://www.linkedin.com/in/martin-pezet-36306a181/" className="socialItem">
+        <a href="https://www.linkedin.com/in/martin-pezet-36306a181/" className="socialItem scaleOnHover">
           <img src={linkedin} alt="LinkedIn Logo" />
         </a>
       </div>
