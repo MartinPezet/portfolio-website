54eb59fdde0b046f000ef0b086511d7c99ea3891
Martin Pezet <mpezet@ntlworld.com>
2023-01-07 10:44:10 +0000

    Added SignIn and SignUp Components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,6 @@
 export { default as About } from "./about/About";
 export { default as Project } from "./project/Project";
 export { default as Navbar } from "./navbar/Navbar";
-export { default as Popup } from "./popup/Popup"
+export { default as Popup } from "./popup/Popup";
+export { default as SignIn } from "./signIn/SignIn";
+export { default as SignUp } from "./signUp/SignUp";
---
+++ client/src/components/signIn/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './signIn.css';
+
+const SignIn = () => {
+  return (
+    <section>SignIn</section>
+  )
+}
+
+export default SignIn
