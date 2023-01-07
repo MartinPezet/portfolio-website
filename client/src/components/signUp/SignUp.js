9d6bed7194e33b6e426462fd5af4feb445a08aa0
Martin Pezet <mpezet@ntlworld.com>
2023-01-07 19:52:43 +0000

    Added Form Content

--- client/src/components/signUp/SignUp.js
+++ client/src/components/signUp/SignUp.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+
+import './signUp.css';
 
 const SignUp = () => {
   return (
-    <section>SignUp</section>
+    <section className="signUp">
+
+        <div className="signUpTitle popUpTitle">
+            <h1 className="gradientText">Register</h1>
+        </div>
+        
+        <form className="signUpForm">
+            <div className="signUpFormContent popUpInput">
+                <input type="email" placeholder="Email" autoComplete='email' />
+                <input type="password" id="pwd" placeholder="Password" />
+                <input type="password" id="confirmPwd" placeholder="Retype Password" />
+            </div>
+            <div className="signUpFormSubmit">
+                <input type="submit" value="Submit" />
+            </div>
+        </form>
+
+    </section>
   )
 }
 
