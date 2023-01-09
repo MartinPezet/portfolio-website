25de30a24edf7fded97f4b648dc472aa08831beb
Martin Pezet <mpezet@ntlworld.com>
2023-01-09 11:00:40 +0000

    Adding Styles for Sign In Modal

--- client/src/components/signIn/SignIn.js
+++ client/src/components/signIn/SignIn.js
@@ -4,7 +4,24 @@ import './signIn.css';
 
 const SignIn = () => {
   return (
-    <section>Sign In</section>
+    <section>
+      <div className="signInTitle popupTitle">
+            <h1 className="gradientText">Sign In</h1>
+        </div>
+        
+        <form className="signInForm">
+            <div className="signInFormContent popupInput">
+                <input type="email" placeholder="Email" autoComplete='email' />
+                <input type="password" id="pwd" placeholder="Password" />
+            </div>
+            <div className="signInRegisterLink">
+              <p>Don't have an account already? Register link</p>
+            </div>
+            <div className="signInFormSubmit popupSubmit">
+                <button className="scaleOnHover">Sign In</button>
+            </div>
+          </form>
+    </section>
   )
 }
 
