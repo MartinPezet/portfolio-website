e516afe14a50d6907515a4d472b58458e298f414
Martin Pezet <mpezet@ntlworld.com>
2023-01-10 16:19:04 +0000

    Changed User model structure

--- signInServer/dataTypes/User.model.js
+++ signInServer/dataTypes/User.model.js
@@ -1,11 +1,25 @@
+const { Int32 } = require("mongodb");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: {
+    userName: {
         type: String,
         required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    permissionLevel: {
+        type: Int32,
+        required: true
     }
+
 });
 
 const User = mongoose.model("user", userSchema);
