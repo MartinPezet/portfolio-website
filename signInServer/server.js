764d36ca389edc35d5eb3e2501281e0c7c8b6a96
Martin Pezet <mpezet@ntlworld.com>
2023-01-10 13:11:07 +0000

    Added User Controller and Model

---
+++ signInServer/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+const UserController = require('./Controllers/User.Controller');
+var dotenv = require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res, next) => {
+    res.json({message: 'Test worked'});
+});
+
+app.get("/users", UserController.getAllUsers);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, console.log("Server listening port", PORT));
