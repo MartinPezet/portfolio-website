5dce5deb9593f0863ddb304f6ddad923a2f6e35f
Martin Pezet <mpezet@ntlworld.com>
2023-02-05 22:28:22 +0000

    Addin Login Route with axios call

--- client/src/components/signIn/SignIn.js
+++ client/src/components/signIn/SignIn.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
 import './signIn.css';
 
 const SignIn = () => {
+
+  const GoogleOauth = () => {
+    console.log("Test")
+
+    // Actually redirect to nodejs server not ann axios call
+  
+    axios.get('http://localhost:3001/oauth/google').then(res => {
+      console.log(res);
+
+    }).catch(err => {
+      console.log(err);
+      
+    })
+  };
+
   return (
     <section>
       <div className="signInTitle popupTitle">
             <h1 className="gradientText">Sign In</h1>
         </div>
+
+        <button onClick={GoogleOauth()}>Google</button>
         
         <form className="signInForm">
             <div className="signInFormContent popupInput">
