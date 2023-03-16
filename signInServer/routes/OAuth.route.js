ceec83e60bcc2d13297886ea3e2c2f5130d67183
Martin Pezet <mpezet@ntlworld.com>
2023-03-16 13:55:47 +0000

    Changed success endpoint to respond with message after user not logged in

--- signInServer/routes/OAuth.route.js
+++ signInServer/routes/OAuth.route.js
@@ -7,7 +7,8 @@ router.get('/google', passport.authenticate("google", {scope:["profile", "email"
 
 router.get('/google/callback', passport.authenticate("google", {
     successRedirect: "http://localhost:3000/",
-    failureRedirect: "/login/failed",
+    failureRedirect: "/login/failed", // Can replace this with frontend URL and use the below fine
+    // failureMesaage: "Authentication failed"
 }));
 
 router.get('/login/failed', (req, res) => {
@@ -19,13 +20,19 @@ router.get('/login/failed', (req, res) => {
 
 router.get('/login/success', (req, res) => {
     if (req.user) {
+        console.log(req.user.displayName, " accessed their data");
         res.status(200).json({
             success: true,
             message: "Logged In",
             user: req.user
         });
+    } else {
+        console.log("No req.user", req.user);
+        res.status(401).json({
+            message: "Not logged in"
+        });
     }
-});
+}); // Get all session data from this endpoint and 
 
 router.get('/logout', (req, res) => {
     req.logOut(function(err) {
