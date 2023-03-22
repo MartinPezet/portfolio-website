750a27d34682ad1cfec127bbc8997af620a435eb
Martin Pezet <mpezet@ntlworld.com>
2023-03-22 17:16:42 +0000

    Added necessary env changes

--- signInServer/config/passport.js
+++ signInServer/config/passport.js
@@ -2,10 +2,12 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 const passport = require('passport');
 const User = require("../dataTypes/User.model");
 
+const callback_URL = process.env.FRONTEND_BASE_URI + "/auth/google/callback";
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_OAUTH_CLIENT_ID,
     clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: callback_URL,
   },
   async function(accessToken, refreshToken, profile, done) {
     // console.log(profile);
