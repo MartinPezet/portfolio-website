e82f4ac42ebcefddfa808daae70993233378997c
Martin Pezet <mpezet@ntlworld.com>
2023-03-22 00:20:00 +0000

    Added Auth check

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import UserProvider from './context/UserProvider'
+import useUser from './hooks/useUser';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -10,17 +10,51 @@ import './App.css';
 
 const App = () => {
 
+  const { user, setUser } = useUser();
+
+  const config = {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      Accept: 'application/json',
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": true
+    }
+  };
+
+  const GetUser = () => {
+    fetch('http://localhost:3001/auth/login/success', config).then(res => { // ++++++++++++++++++++++++ CHANGE TO AXIOS ++++++++++++++++++++++++
+      if (res.status === 200) return res.json()
+    }).then(resObj => {
+      if (resObj.success) {
+        const newUser = {
+          displayName: resObj.user.displayName,
+          name: resObj.user.name,
+          email: resObj.user.email,
+          pictureLink: resObj.user.pictureLink
+        }
+        setUser(newUser)
+      } else {
+        console.error(resObj.message);
+      }
+    }).catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    if (Object.keys(user)?.length === 0) {
+      GetUser();
+    }
+  })
+
   return (
-    <UserProvider>
-      <Router>
-        <section className='App'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route exact path='/sign-in' element={<SignInPage />} />   
-          </Routes>
-        </section>
-      </Router>
-    </UserProvider>
+    <Router>
+      <section className='App'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route exact path='/sign-in' element={<SignInPage />} />   
+        </Routes>
+      </section>
+    </Router>
   )
 }
 
