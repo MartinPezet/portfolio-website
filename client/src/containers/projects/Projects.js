f5ff3441cfa076b4ad02a78604e92cf7b21b5bb6
Martin Pezet <mpezet@ntlworld.com>
2023-12-05 19:25:00 +0000

    Finish Projects Section overhaul

--- client/src/containers/projects/Projects.js
+++ client/src/containers/projects/Projects.js
@@ -3,19 +3,37 @@ import React from 'react';
 import './projects.css';
 import { Project } from '../../components'
 
-// Images
-import placeholder from '../../assets/projectsComingSoon.png';
-
 
 const Projects = () => {
   return (
     <section className="projects" id="projects">
       <div className="projectsTitle">
-        <p className="gradientText">Hosted Projects</p>
+        <h2 className="gradientText">Project Updates.</h2>
+        <h2 className="gradientText">Keep up to date with how I'm getting on.</h2>
       </div>
       <div className="projectsContent">
-      <Project direction='left' title="Real Estate Supervisor" text="A real estate management system which will cover the whole experience of a property manager, from contacting tenants about queries to calling out engineers for work." image={placeholder} imgAlt="This project is coming soon" />
-      <Project direction='right' title="Headache Decision Map" text="This is a Java-React app that goes through a decision map on Java depending on the results of the questions stated about Primary headaches." image={placeholder} imgAlt="This project is coming soon" />
+        <div className="mostRecentProject">
+          <Project 
+            title="Real Estate Supervisor" 
+            date={new Date("2023-07-01")}
+            text="A real estate management system which will cover the whole experience of a property manager, from contacting tenants about queries to calling out engineers for work." 
+          />
+        </div>
+        <div className="otherProjects">
+          <Project 
+            title="Headache Decision Map" 
+            text="This is a Java-React app that goes through a decision map on Java depending on the results of the questions stated about Primary headaches."
+          />
+          <Project 
+            title="CMS System"
+          />
+          <Project 
+            title="Global SCSS" 
+          />
+          <Project 
+            title="Python Scripting"
+          />
+        </div>
       </div>
       
     </section>
