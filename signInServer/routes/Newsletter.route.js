cc3eeb8c44b7ba809b3af73029f53c934994af00
Martin Pezet <mpezet@ntlworld.com>
2024-02-21 22:28:29 +0000

    ✨PORT-4: Add backend server newsletter functionality

---
+++ signInServer/routes/Newsletter.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const NewsletterController = require('../controllers/Newsletter.controller');
+
+router.get("", NewsletterController.countAllEmailSignUps);
+router.post("", NewsletterController.newsletterSignUp);
+router.get("/pictures", NewsletterController.getEmailPictures);
+
+//DEV ONLY
+router.post("/clear", NewsletterController.clearNewsletterSignUps);
+router.get("/sign-ups", NewsletterController.getAllNewsletterSignUps);
+
+module.exports = router;
