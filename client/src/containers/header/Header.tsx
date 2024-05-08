0f9df147b06b400840926c7565d93368b532389c
Martin Pezet <mpezet@ntlworld.com>
2024-05-08 20:22:02 +0100

    ✨PORT-2: Change same page scroll functionality

--- client/src/containers/header/Header.tsx
+++ client/src/containers/header/Header.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './header.css';
 
 const Header: React.FC = () => {
+
+  const scrollTo = (id) => {
+    document.getElementById(id).scrollIntoView();
+  };
+
   return (
     <section className="" id="home">
       <div className="hero">
         <h1 className="uppercase text-9xl font-semibold mb-6">Martin Pezet</h1>
         <p className="text-2xl mb-8">Full-stack Software Engineer</p>
-        <a className="button scaleOnHover" href="#projects">Projects</a>
+        <button className="button scaleOnHover" onClick={() => scrollTo('projects')}>Projects</button>
       </div>
     </section>
   )
