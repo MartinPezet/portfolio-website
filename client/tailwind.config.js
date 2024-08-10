719762be278b23c72236f7adb9e680c83118e933
Martin Pezet <mpezet@ntlworld.com>
2024-08-10 18:41:12 +0100

    🔧PORT-2: Add tailwind colours #time 1h

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,22 +1,26 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     content: [
         "./src/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
-        extend: {},
-        colors: {
-            'primary': {
-                50: '#e9f9ff',
-                100: '#cef1ff',
-                200: '#a7e8ff',
-                300: '#6bdeff',
-                400: '#26c7ff',
-                500: '#009fff',
-                600: '#0075ff',
-                700: '#005aff',
-                800: '#004de6',
-                900: '#0047b3',
-                950: '#001533',
+        extend: {
+            colors: {
+                ...colors,
+                'primary': {
+                    50: '#e9f9ff',
+                    100: '#cef1ff',
+                    200: '#a7e8ff',
+                    300: '#6bdeff',
+                    400: '#26c7ff',
+                    500: '#009fff',
+                    600: '#0075ff',
+                    700: '#005aff',
+                    800: '#004de6',
+                    900: '#0047b3',
+                    950: '#001533',
+                }
             }
         }
     },
