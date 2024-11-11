7b9a143a0867d00c8dc7ee5b1071bcd671a639bd
Martin Pezet <mpezet@ntlworld.com>
2024-11-11 23:26:45 +0000

    ✨PORT-2: Add current working location to header

--- client/src/containers/header/Header.tsx
+++ client/src/containers/header/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 const Header: React.FC = () => {
 
@@ -10,7 +12,8 @@ const Header: React.FC = () => {
   return (
     <section className="" id="home">
       <div className="hero">
-        <h1 className="uppercase text-9xl font-semibold mb-6">Martin Pezet</h1>
+        <h1 className="uppercase text-9xl font-semibold mb-5">Martin Pezet</h1>
+        <p className="text-slate-300"><FontAwesomeIcon icon={faLocationDot}/> Hampshire, United Kingdom</p>
         <p className="text-2xl mb-8">Full-stack Software Engineer</p>
         <button className="button scaleOnHover" onClick={() => scrollTo('projects')}>Projects</button>
       </div>
