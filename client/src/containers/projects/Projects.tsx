c75f020c636b1b1bd5efa6fea0c292ace190479f
Martin Pezet <mpezet@ntlworld.com>
2024-11-12 21:02:09 +0000

    💬PORT-2: Add new project from Github

--- client/src/containers/projects/Projects.tsx
+++ client/src/containers/projects/Projects.tsx
@@ -14,6 +14,11 @@ const Projects: React.FC = () => {
 
       <div className="max-w-7xl grid grid-cols-3 gap-8">
         <Project 
+          title="Local First To Do"
+          text="Investigation of using a Local First architecture using Automerge library for conflict management."
+          codeLink="https://github.com/MartinPezet/local-first-to-do"
+        />
+        <Project 
           title="Headache Decision Map"
           text="An app that can be first line of diagnostics towards primary headaches as it guides you through a series of questions to give you an estimate of your headache."
           codeLink="https://github.com/MartinPezet/headacheDecisionMap"
