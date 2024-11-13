2bb11646705d1f591fbf600fd985b319adcd9263
Martin Pezet <mpezet@ntlworld.com>
2024-11-13 22:32:25 +0000

    💄PORT-2: Experiment with Bento Grid layout

--- client/src/containers/aboutMe/AboutMe.tsx
+++ client/src/containers/aboutMe/AboutMe.tsx
@@ -7,13 +7,29 @@ import './aboutMe.css';
 
 const AboutMe: React.FC = () => {
   return (
-    <section className="flex flex-col items-center gap-8" id="about-me">
-      <div className="max-w-7xl w-full grid grid-cols-3 gap-8">
-        <img src={headshot} className="about-me-img rounded-2xl" alt="Headshot of Martin Pezet"/>
-        <div className="card col-span-2">
-          <h2 className="text-4xl mb-4 hover:underline decoration-primary-600 decoration-wavy decoration-4">About Me</h2>
-          <p>You'll learn how I'm really not good at posing for photos.</p>
-          <p>Bento Grid with Education and Hobbies</p>
+    <section className="flex flex-col items-center gap-8 mb-36" id="about-me">
+      <div className="max-w-6xl w-full grid grid-cols-6 grid-rows-4 gap-6">
+        <img src={headshot} className="about-me-img rounded-2xl col-span-2 row-span-2" alt="Headshot of Martin Pezet"/>
+        <div className="card col-span-2 row-span-1 flex justify-center items-center">
+          <h2 className="text-4xl uppercase font-semibold tracking-wide">About Me</h2>
+        </div>
+        <div className="card col-span-2 row-span-2">
+          <p>Hobbies & Interests</p>
+        </div>
+        <div className="card col-span-2 row-span-2">
+          Hover over/focus/click card to flip and reveal the topic of the card
+        </div>
+        <div className="card col-span-2 row-span-1">
+          <p>Hobbies & Interests</p>
+        </div>
+        <div className="card col-span-2 row-span-3">
+          Image
+        </div>
+        <div className="card col-span-3 row-span-2">
+          Values (Not repeating the same mistake, )
+        </div>
+        <div className="card col-span-1 row-span-2">
+          CV Download?
         </div>
       </div>
     </section>
