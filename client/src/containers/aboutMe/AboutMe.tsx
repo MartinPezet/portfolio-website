e79c82bfe2f51f343c8aeda3a5c9841fbbbd43a7
Martin Pezet <mpezet@ntlworld.com>
2024-11-19 22:50:30 +0000

    💄PORT-2: Make Bento more appealing in about me section

--- client/src/containers/aboutMe/AboutMe.tsx
+++ client/src/containers/aboutMe/AboutMe.tsx
@@ -8,28 +8,32 @@ import './aboutMe.css';
 const AboutMe: React.FC = () => {
   return (
     <section className="flex flex-col items-center gap-8 mb-36" id="about-me">
-      <div className="max-w-6xl w-full grid grid-cols-6 grid-rows-4 gap-6">
-        <img src={headshot} className="about-me-img rounded-2xl col-span-2 row-span-2" alt="Headshot of Martin Pezet"/>
+      <div className="max-w-6xl w-full grid grid-cols-6 lg-custom-rows gap-6">
+        <img src={headshot} className="about-me-img rounded-2xl col-span-2 row-span-3" alt="Headshot of Martin Pezet"/>
         <div className="card col-span-2 row-span-1 flex justify-center items-center">
           <h2 className="text-4xl uppercase font-semibold tracking-wide">About Me</h2>
         </div>
-        <div className="card col-span-2 row-span-2">
-          <p>Hobbies & Interests</p>
-        </div>
-        <div className="card col-span-2 row-span-2">
-          Hover over/focus/click card to flip and reveal the topic of the card
-        </div>
-        <div className="card col-span-2 row-span-1">
+        <div className="card col-span-2 row-span-3">
           <p>Hobbies & Interests</p>
         </div>
         <div className="card col-span-2 row-span-3">
-          Image
+          Hover over/focus/click card to flip and reveal the topic of the card
+        </div>
+        <div className="card col-span-2 row-span-1">
+          <p></p>
+        </div>
+        <div className="card col-span-2 row-span-2">
+          Image within <a href="https://uiverse.io/mobinkakei/heavy-bulldog-78" target="_blank">Phone card</a>
         </div>
         <div className="card col-span-3 row-span-2">
           Values (Not repeating the same mistake, )
+          Cool idea for this <a href="https://uiverse.io/dylanharriscameron/stupid-mole-90" target="_blank">card</a>
         </div>
         <div className="card col-span-1 row-span-2">
           CV Download?
+        </div>
+        <div className="card col-span-2 row-span-1">
+          
         </div>
       </div>
     </section>
