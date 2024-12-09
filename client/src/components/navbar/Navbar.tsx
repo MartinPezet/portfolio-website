f7bbb9d4b276e99fa71223c5ecb5372e98e5db1b
Martin Pezet <mpezet@ntlworld.com>
2024-12-09 21:03:09 +0000

    💄PORT-2: Improve Navbar reactivity

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -30,47 +30,46 @@ const Navbar: React.FC = () => {
   )
 
   const SocialsNav: React.FC = () => (
-    <div className="flex flex-row items-center gap-8">
+    <>
       <a href={config.personalLinks.github} target="_blank" rel="noreferrer">
         <FontAwesomeIcon className="tech-icon hover:text-[#4078c0]" icon={faGithub}/>
       </a>
       <a href={config.personalLinks.linkedIn} target="_blank" rel="noreferrer">
       <FontAwesomeIcon className="tech-icon hover:text-[#0077B5]" icon={faLinkedin}/>
       </a>
-    </div>
+    </>
   )
 
   // TODO: When scrolled past the navbar the navbar will drop down like an apple notch bar
 
   return (
-    <nav className="sticky top-4 z-10 flex flex-row items-center justify-between max-w-7xl w-100 gap-12 mx-auto py-4 px-8 my-4 rounded-xl bg-primary-950 bg-opacity-50" id="navbar">
-      {/* TODO Remove opacity and make logo smaller once scrolled */}
-      <div className="cursor-pointer flex justify-center items-center" onClick={() => onInternalNavigationClick('home')}>
-        <p className="title-font logo fugaz">M</p>
-      </div>
-      <div className="flex flex-row text-xl font-semibold sm:gap-8 md:gap-12">
-        <Menu />
-      </div>
-      <div className="navbar-socials">
-        <SocialsNav/>
-      </div>
-      <div className="navBarMobMenu">
-        {toggleMobMenu
-          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMobMenu(false)} />
-          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMobMenu(true)} />
-        }
-        {toggleMobMenu && (
-           <div className='navBarMobMenuContainer scaleUpCenter'> {/* Change scaleUpCenter to scroll from top animation */}
-            <div className='navBarMobMenuContainerLinks'>
-              <Menu />
-              <div className='navBarMobMenuContainerLinksSignIn'>
-                <SocialsNav/>
+    <div className="sticky top-4 z-10 flex justify-center">
+      <nav className="relative flex flex-row items-center justify-between w-full gap-4 sm:gap-12 mx-4 my-4 max-w-7xl py-4 px-4 sm:px-8 rounded-xl bg-primary-950 bg-opacity-50" id="navbar">
+        {/* TODO Remove opacity and make logo smaller once scrolled */}
+        <div className="cursor-pointer flex justify-center items-center" onClick={() => onInternalNavigationClick('home')}>
+          <p className="title-font logo fugaz">M</p>
+        </div>
+        <div className="flex flex-row text-xl font-semibold gap-8 sm:gap-12 max-md:hidden">
+          <Menu />
+        </div>
+        <div className="flex flex-row items-center gap-8 max-xs:hidden">
+          <SocialsNav/>
+        </div>
+        <div className="md:hidden navBarMobMenu">
+          {toggleMobMenu}
+          {toggleMobMenu && (
+            <div className="absolute flex flex-col justify-center w-11/12 bg-primary-950 p-8 top-full left-2/4 translate-x-[-50%] rounded-b-xl gap-4"> {/* Change scaleUpCenter to scroll from top animation */}
+              <div className="flex flex-col text-right gap-4">
+                <Menu />
+                <div className="flex flex-col xs:hidden">
+                  <SocialsNav/>
+                </div>
               </div>
             </div>
-          </div>
-        )}
-      </div>
-    </nav>
+          )}
+        </div>
+      </nav>
+    </div>
   )
 }
 
