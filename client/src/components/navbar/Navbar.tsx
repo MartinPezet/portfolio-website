46e69e97a8ca06979aaed99cca72e55d57830f28
Martin Pezet <mpezet@ntlworld.com>
2024-12-09 23:55:11 +0000

    💄PORT-2: Finish Navbar hamburger and proper alignment

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import './navbar.css';
 
@@ -9,7 +9,7 @@ import config from "../../utils/global-constants.json";
 
 // Mobile Nav Dependenciestrue,
 import { useState } from 'react';
-import {scrollToId} from '../../utils/pageHelpers';
+import { scrollToId } from '../../utils/pageHelpers';
 
 const Navbar: React.FC = () => {
 
@@ -34,28 +34,43 @@ const Navbar: React.FC = () => {
         <FontAwesomeIcon className="tech-icon hover:text-[#4078c0]" icon={faGithub}/>
       </a>
       <a href={config.personalLinks.linkedIn} target="_blank" rel="noreferrer">
-      <FontAwesomeIcon className="tech-icon hover:text-[#0077B5]" icon={faLinkedin}/>
+        <FontAwesomeIcon className="tech-icon hover:text-[#0077B5]" icon={faLinkedin}/>
       </a>
     </>
   )
 
-  // TODO: When scrolled past the navbar the navbar will drop down like an apple notch bar
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 0);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <div className="sticky top-4 z-10 flex justify-center">
-      <nav className="relative flex flex-row items-center justify-between w-full gap-4 sm:gap-12 mx-4 my-4 max-w-7xl py-4 px-4 sm:px-8 rounded-xl bg-primary-950 bg-opacity-50" id="navbar">
-        {/* TODO Remove opacity and make logo smaller once scrolled */}
-        <div className="cursor-pointer flex justify-center items-center" onClick={() => onInternalNavigationClick('home')}>
-          <p className="title-font logo fugaz">M</p>
+    <header className={`header sticky z-10 flex justify-center${hasScrolled ? " top-2" : " top-4"}`}>
+      <nav className="relative flex flex-row items-center justify-between w-full gap-4 sm:gap-12 mx-4 mb-4 max-w-7xl py-4 px-4 sm:px-8 rounded-xl bg-[#010e25]" id="navbar">
+        <div className="cursor-pointer flex items-center w-14 md:w-24" onClick={() => onInternalNavigationClick('home')}>
+          <p className={`title-font logo fugaz${hasScrolled ? " fugaz-scrolled" : ""}`}>M</p>
         </div>
         <div className="flex flex-row text-xl font-semibold gap-8 sm:gap-12 max-md:hidden">
           <Menu />
         </div>
-        <div className="flex flex-row items-center gap-8 max-xs:hidden">
+        <div className="flex flex-row items-center md:justify-end gap-8 max-xs:hidden md:w-24">
           <SocialsNav/>
         </div>
-        <div className="md:hidden navBarMobMenu">
-          {toggleMobMenu}
+        <div className="md:hidden flex justify-end w-14">
+          <button className={`hamburger ${toggleMobMenu ? "selected" : ""}`} onClick={() => setToggleMobMenu(!toggleMobMenu)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
           {toggleMobMenu && (
             <div className="absolute flex flex-col justify-center w-11/12 bg-primary-950 p-8 top-full left-2/4 translate-x-[-50%] rounded-b-xl gap-4"> {/* Change scaleUpCenter to scroll from top animation */}
               <div className="flex flex-col text-right gap-4">
@@ -68,7 +83,7 @@ const Navbar: React.FC = () => {
           )}
         </div>
       </nav>
-    </div>
+    </header>
   )
 }
 
