f7163fdb6574d31d49d860acba33d0dfd0d42ad3
Martin Pezet <mpezet@ntlworld.com>
2024-12-28 09:53:15 +0000

    💄PORT-2: Improve hover for tech icons

--- client/src/containers/footer/Footer.tsx
+++ client/src/containers/footer/Footer.tsx
@@ -9,9 +9,15 @@ const Footer: React.FC = () => {
       <div className="flex flex-col gap-2 md:gap-4 items-center">
         <h2 className="uppercase text-center text-lg">Message Me</h2>
         <div className="flex gap-6 items-center">
-          <a href="mailto:martinpezet@ntlworld.com"><FontAwesomeIcon icon={faEnvelope} className="h-8 hover:text-[#1A73E8]" aria-label="Envelope to make email"/></a>
-          <a href="https://wa.me/07806519657"><FontAwesomeIcon icon={faWhatsapp} className="h-8 hover:text-[#25D366]" aria-label="Whatsapp logo"/></a>
-          <a href="https://www.linkedin.com/messaging/thread/new/?recipients=martin-pezet-36306a181"><FontAwesomeIcon icon={faLinkedin} className="h-8 hover:text-[#0077B5]"/></a>
+          <a href="mailto:martinpezet@ntlworld.com">
+            <FontAwesomeIcon icon={faEnvelope} className="tech-icon h-8 hover:text-[#1A73E8]" aria-label="Envelope to make email"/>
+            </a>
+          <a href="https://wa.me/07806519657" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faWhatsapp} className="tech-icon h-8 hover:text-[#25D366]" aria-label="Whatsapp logo"/>
+          </a>
+          <a href="https://www.linkedin.com/messaging/thread/new/?recipients=martin-pezet-36306a181" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} className="tech-icon h-8 hover:text-[#0077B5]"/>
+          </a>
         </div>
       </div>
       
